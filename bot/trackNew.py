2ff6e441ea711df6306bce97ba476a117550a2a6
JR (zen) Courtois <jr@jrcourtois.net>
2017-06-03 17:33:12 +0200

    track orphan with no link

--- bot/trackNew.py
+++ bot/trackNew.py
@@ -17,6 +17,7 @@ r = api.APIRequest(site,params)
 result = r.query(False)
 nbAdopted = 0
 nbLinkAdded = 0
+nbOrphan = 0
 for p in result['query']['recentchanges']:
 	
 	page = OrphanPage(p['title'])
@@ -24,13 +25,13 @@ for p in result['query']['recentchanges']:
 	Tools.printProgress(nbAdopted + nbLinkAdded, 400)
 	if (page.getNbLinks() > 2):
 		nbAdopted += 1
+	elif (page.getNbLinks() > 0):
+		nbOrphan += 1
 	else:
 		try:
 			nbLinkAdded += Tools.setOrphanIfNeeded(page)
 		except:
 			print "Exception occurs: %s" % p['title'] 
 
-print "==== %d orphelins, %d adoptés" % (nbLinkAdded, nbAdopted)
-
-
+print "==== %d orphelins, %d links added, %d adoptés" % (nbOrphan, nbLinkAdded, nbAdopted)
 
