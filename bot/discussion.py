54e8d2ed0f43ee1c1ab28f64249dbf7cfc7058b9
JR (zen) Courtois <jr@jrcourtois.net>
2017-06-04 19:29:44 +0200

    python v3

--- bot/discussion.py
+++ bot/discussion.py
@@ -13,25 +13,26 @@ cons = Page(site, "Utilisateur:Jrcourtois/SuiviSuppressions/conservations")
 
 liste = disc.getWikiText()
 
-page = [u""]
+page = [""]
 articles = {}
 indice = 0
 title = ""
 rePar = re.compile(r"==.*==")
 reTitle= re.compile(r"\{\{(.*)\|1=(.*?)\}\}")
 reDate = re.compile(r"(\d+\s.*?\s201.).*\d+\:\d\d\s\(CES?T\)$")
-for l in liste.splitlines():
+for line in liste.splitlines():
+	l = line.decode("utf8")
 	if rePar.match(l):
 		indice += 1
-		page.insert(indice, l.decode("utf8")+ "\n".decode("utf8")) 
+		page.insert(indice, l + "\n") 
 	else:
 		if (page[indice]):
-			page[indice]+=  l.decode("utf8") + "\n".decode("utf8")
+			page[indice]+=  l + "\n"
 
 	m = reTitle.search(l)
 	if m:
 		title = m.group(2)
-		print title
+		print(title)
 	m = reDate.search(l)
 	if m:
 		if title:
@@ -41,12 +42,13 @@ for l in liste.splitlines():
 
 props = prop.getWikiText().decode("utf8")
 for l in articles:
-	props +="\n#{{/prop|" + l.decode("utf8") + "|" + articles[l].decode("utf8")+ "}}"
+	props +="\n#{{/prop|%s|%s}}" % (l, articles[l])
 
 lines = props.split("\n")
-newProps = u""
-appSupp = u""
-appCons = u""
+newProps = ""
+appSupp = ""
+appCons = ""
+
 i = 0
 for line in lines:
 	#Tools.printProgress(i,len(lines))
@@ -55,30 +57,30 @@ for line in lines:
 	if m:
 		p = Page(site, "Discussion:" + m.group(1) + "/Suppression")
 		try:
-			txt  = p.getWikiText()
+			txt  = p.getWikiText().decode("utf8")
 			if (txt.find("{{Article supp")>-1):
-				appSupp += u"\n#{{/supp|" + m.group(1) + "|" + m.group(2) + "|" + time.strftime("%d/%m/%Y") + "}}"
+				appSupp += "\n#{{/supp|" + m.group(1) + "|" + m.group(2) + "|" + time.strftime("%d/%m/%Y") + "}}"
 			elif (txt.find("{{Article cons")>-1):
-				appCons += u"\n#{{/cons|" + m.group(1) + "|" + m.group(2) + "|" + time.strftime("%d/%m/%Y") + "}}"
+				appCons += "\n#{{/cons|" + m.group(1) + "|" + m.group(2) + "|" + time.strftime("%d/%m/%Y") + "}}"
 			else:
-				newProps += u"\n" + line
+				newProps += "\n" + line
 		except:
-			print "page not exists"
+			print("**********************************page not exists")
 
-print "----"
-print "Supp"
-supp.edit(appendtext=appSupp.encode("utf8"),bot=True)
-print appSupp
-print "----"
-print "Cons"
-cons.edit(appendtext=appCons.encode("utf8"),bot=True)
-print appCons
-print "----"
-prop.edit(text=newProps.encode("utf8"), summary="added last propositions",bot=True)
-print newProps
-t = u""
+print("***********************************")
+print("Supp")
+supp.edit(appendtext=appSupp,bot=True)
+print(appSupp)
+print("***********************************")
+print("Cons")
+cons.edit(appendtext=appCons,bot=True)
+print(appCons)
+print("***********************************")
+prop.edit(text=newProps, summary="added last propositions",bot=True)
+print(newProps)
+t = ""
 for a in page:
-	t += a + u"\n".encode("utf8")
+	t += a + "\n"
 
 t = re.sub(r"\n\n+","\n\n", t)
-disc.edit(text=t.encode("utf8"),bot=True, summary="avertissement de suppressions archivés")
+disc.edit(text=t,bot=True, summary="avertissement de suppressions archivés")
