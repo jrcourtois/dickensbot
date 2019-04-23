2f0cfd18f4ce6861600ce2e446d1dc534a5c686a
JR Courtois.net <liste@jrcourtois.net>
2019-04-23 08:46:56 +0200

    password

--- lib/Site.py
+++ lib/Site.py
@@ -3,7 +3,7 @@ from wikitools import wiki
 # create a Wiki object
 site = wiki.Wiki("https://fr.wikipedia.org/w/api.php")
 # login - required for read-restricted wikis
-b = site.login("DickensBot", "AQWzsx123")
+b = site.login("DickensBot", "le bot a un nouveau mot de passe")
 en_site = wiki.Wiki("https://en.wikipedia.org/w/api.php")
 
 wikt = wiki.Wiki("https://fr.wiktionary.org/w/api.php")
