f9ba428b3b16e075b93c654735bf64ed7c8f425c
JR Courtois <liste@jrcourtois.net>
2021-12-17 23:05:47 +0100

    applyReport.sh

--- applyReport.sh
+++ applyReport.sh
@@ -2,8 +2,17 @@
 
 export PYTHONPATH=/home/jr/dev/wiki/dickensbot/lib:/home/jr/dev/wiki/wikitools
 
-python3 bot/orphan.py
-python3 bot/adopt.py
+echo "Mise à jour de la page de Discussion de JrCourtois - retrait des avertissements de pdd"
 python3 bot/discussion.py
+echo "Mise à jour des pages projets recensant les pages orphelines"
+python3 bot/buildProjectPagesShort.py
+echo "Parcours de tous les orphelins"
+python3 bot/parseAllOrphans.py
+echo "Récupération des orphelins de la page des orphelins"
+python3 bot/orphan.py
+echo "Adoption des orphelins tels que remontés via parseAllOrphans"
+python3 bot/adopt.py
+echo "Recherche des orphelins récemment créés"
 python3 bot/trackNew.py
+echo "Mise à jour des pages mensuelles d'adoption/tentative"
 python3 bot/analysisCompare.py
